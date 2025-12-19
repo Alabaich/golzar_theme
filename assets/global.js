d4082824e35e276519ba04a88d189e81fc880f58
Alabaika <enjoyable.design@gmail.com>
2025-12-19 17:03:00 -0500

    fix slider

--- assets/global.js
+++ assets/global.js
@@ -605,15 +605,15 @@ class ModalDialog extends HTMLElement {
     this.addEventListener('keyup', (event) => {
       if (event.code.toUpperCase() === 'ESCAPE') this.hide();
     });
-    if (this.classList.contains('media-modal')) {
-      this.addEventListener('pointerup', (event) => {
-        if (event.pointerType === 'mouse' && !event.target.closest('deferred-media, product-model')) this.hide();
-      });
-    } else {
-      this.addEventListener('click', (event) => {
-        if (event.target === this) this.hide();
-      });
-    }
+    // if (this.classList.contains('media-modal')) {
+    //   this.addEventListener('pointerup', (event) => {
+    //     if (event.pointerType === 'mouse' && !event.target.closest('deferred-media, product-model')) this.hide();
+    //   });
+    // } else {
+    //   this.addEventListener('click', (event) => {
+    //     if (event.target === this) this.hide();
+    //   });
+    // }
   }
 
   connectedCallback() {
